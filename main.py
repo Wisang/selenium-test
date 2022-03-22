2ce9365408e20d90a670d7526001c65c5b7a77f7
wisang <wseom7@gmail.com>
2022-03-23 00:25:47 +0900

    xpath works!

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ chrome_driver_path = "D:\dev-tools\chromedriver.exe"
 s = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=s)
 # driver = webdriver.Chrome(executable_path=chrome_driver_path)
-driver.get('https://www.amazon.com/Fitbit-Inspire-Fitness-Tracker-Included/dp/B08DFDP1TR/ref=sr_1_4?crid=2ZKPHWC7E8LIL&keywords=fitbit&qid=1647961526&sprefix=fitbit%2Caps%2C265&sr=8-4')
-price = driver.find_element(By.XPATH, '//*[@id="corePrice_desktop"]/div/table/tbody/tr[2]/td[2]/span[1]/span[2]')
+driver.get('https://www.amazon.com/Fitbit-Advanced-Management-Tracking-Graphite/dp/B09BXQ4HMB/ref=sr_1_3?crid=2ZKPHWC7E8LIL&keywords=fitbit&qid=1647961526&sprefix=fitbit%2Caps%2C265&sr=8-3')
+price = driver.find_element(By.XPATH, '//*[@id="olp_feature_div"]/div[2]/span/a/span[2]')
 
 print(price.text)
 
