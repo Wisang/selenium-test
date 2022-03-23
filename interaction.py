7da179b691daa43b14299d2c70d302a51761e076
wisang <wseom7@gmail.com>
2022-03-24 00:05:11 +0900

    completed using try except

--- interaction.py
+++ interaction.py
@@ -7,7 +7,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-chrome_driver_path = "/Users/wisangeom/dev-util/chromedriver"
+chrome_driver_path = "D:\dev-tools\chromedriver.exe"
+# chrome_driver_path = "/Users/wisangeom/dev-util/chromedriver"
 
 s = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=s)
@@ -15,34 +16,33 @@ url = 'https://orteil.dashnet.org/experiments/cookie/'
 driver.get(url)
 
 cookie = driver.find_element(By.ID, "cookie")
-store = driver.find_elements(By.CSS_SELECTOR, "#store div")
+# store = driver.find_elements(By.CSS_SELECTOR, "#store div")
 
 inner_loop_end = time.time() + 5
-outer_loop_end = time.time() + 60
+outer_loop_end = time.time() + 60*2
 
 while time.time() < outer_loop_end:
     while time.time() < inner_loop_end:
         cookie.click()
 
+    inner_loop_end = inner_loop_end + 5
+
     store = driver.find_elements(By.CSS_SELECTOR, "#store div")
+
     for i in range(len(store)-1, -1, -1):
-        if store[i].get_attribute("class") == "":
-            print(f"{store[i].text} is clicked")
-            store[i].click()
-            break
+        try:
+            if store[i].get_attribute("class") == "":
+                print(f"{store[i].text} is clicked")
+                store[i].click()
+                break
+        except selenium.common.exceptions.StaleElementReferenceException:
+            pass
 
+cps = driver.find_element(By.ID, "cps")
 
-# for item in store:
-#     try:
-#         item_list.append(item.text.split("-")[1].strip())
-#     except IndexError:
-#         pass
+print("click/second is " + cps.text)
 
-
-
-
-
-
+driver.quit()
 
 # count = driver.find_element(By.CSS_SELECTOR, "#articlecount a")
 # arts_link = driver.find_element(By.LINK_TEXT, "The arts")
--- main.py
+++ main.py
@@ -3,16 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-# chrome_driver_path = "D:\dev-tools\chromedriver.exe" for window
-chrome_driver_path = "/Users/wisangeom/dev-util/chromedriver"
+chrome_driver_path = "D:\dev-tools\chromedriver.exe" #for window
+# chrome_driver_path = "/Users/wisangeom/dev-util/chromedriver"
 
 s = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=s)
 url = 'https://www.python.org/'
 driver.get(url)
-
-# menu_path = '//*[@id="content"]/div/section/div[3]/div[2]/div/ul'
-# event_div = driver.find_element(By.XPATH, menu_path)
 
 date_list = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
 title_list = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
